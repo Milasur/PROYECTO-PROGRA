36fcffc20eddc0b7ce6f0947cb0ed03f0f349a24
JosuuCR <jvalles.cr@gmail.com>
2026-05-20 20:54:38 -0600

    prueba 2

--- logica/generador.py
+++ logica/generador.py
@@ -1,1 +1,6 @@
-#Funcion 1: generar
+# Funcion 1: generar
+
+def generar():
+    print("Generando el código...")
+    # Aquí puedes agregar la lógica para generar el código
+    print("Código generado exitosamente.")
