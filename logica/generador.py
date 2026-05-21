72d7123327c1024bed32ff3e1276ddadc782bb57
JosuuCR <jvalles.cr@gmail.com>
2026-05-20 21:57:35 -0600

    FUNCION 4 - PREELIMINAR

--- logica/generador.py
+++ logica/generador.py
@@ -1,11 +1,32 @@
+import random
+
 # Funcion 1: generar
+
 
 def generar():
     print("Generando el código...")
     # Aquí puedes agregar la lógica para generar el código
     print("Código generado exitosamente.")
 
-#Funcion 1: generar
+# Funcion 4 -  Contraseña Robusta
 
 
-sdfsdfsdf
+"""
+ENTRADAS:
+Longitud deseada de la contraeña (int) 
+SALIDAS:
+Una contraseña robusta (str) 
+RESTRICCIONES:
+La longitud debe ser al menos 10 caracteres.
+Debe incluir al menos una letra mayúscula, una letra minúscula, un número y un simbolo ( @ # $ % & *)
+"""
+
+
+def generar_contraseña_robusta(longitud):
+
+    # Casos de prueba:
+
+
+print(generar_contraseña_robusta(12))  # Ejemplo de salida: "A1b@C3d$E5f"
+# Ejemplo de salida: "Error: La longitud debe ser al menos 10 caracteres."
+print(generar_contraseña_robusta(8))
