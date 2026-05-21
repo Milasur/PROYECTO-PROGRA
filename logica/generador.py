a1d3d7e43af720523b4816eb84db910173e825a9
JosuuCR <jvalles.cr@gmail.com>
2026-05-20 20:51:16 -0600

    PRUEBA 1

--- logica/generador.py
+++ logica/generador.py
@@ -1,1 +1,18 @@
-#Funcion 1: generar
+# Funcion 1: generar
+
+
+# Funcion 2: generar_contenido
+
+print("Hola, soy el generador de contenido.")
+
+
+def generar_contenido():
+    print("Generando contenido...")
+    # Aquí puedes agregar la lógica para generar el contenido deseado
+    contenido = "Este es el contenido generado."
+    return contenido
+
+
+if __name__ == "__main__":
+    contenido_generado = generar_contenido()
+    print("Contenido generado:", contenido_generado)
