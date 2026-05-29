279dd325a6ac40d34f2791f69e5770fbbae83c75
rarau <rjaltamiranochaves@gmail.com>
2026-05-28 21:51:08 -0600

    Funcion 9

--- logica/generador.py
+++ logica/generador.py
@@ -205,7 +205,77 @@ def generar_contraseña_passphrase(cantidad_palabras, separador, numero_final):
     contraseña += str(numero_final)
     return contraseña
 
+#Funcion 8: Evaluar la fortaleza de una contraseña
+'''
+ENTRADAS:
+    contraseña (str): contraseña a evaluar
+SALIDAS:
+    Fortaleza de la contraseña (int)
+RESTRICCIONES:
+    La fortaleza se calcula como el número de tipos de caracteres presentes.
+    Tipos de caracteres: mayúsculas, minúsculas, dígitos, símbolos.'''
+    
+def evaluar_fortaleza_contraseña(contraseña):
+    puntuacion = 0
+    if len(contraseña) >= 4 and len(contraseña) <= 5:
+        puntuacion += 1
+    if len(contraseña) >= 6 and len(contraseña) <= 9:
+        puntuacion += 2
+    if len(contraseña) >= 10:
+        puntuacion += 3
+    aparicionesMayusculas = 0
+    
+    for caracteres in contraseña:
+        if caracteres in MAYUSCULAS:
+            aparicionesMayusculas += 1
+    if aparicionesMayusculas == 0:
+        puntuacion += -1
+    elif aparicionesMayusculas >= 1:
+        puntuacion += 1
+    elif aparicionesMayusculas >= 3:
+        puntuacion += 3
 
+    aparicionesMinusculas = 0
+    
+    for caracteres in contraseña:
+        if caracteres in MINUSCULAS:
+            aparicionesMinusculas += 1
+            if aparicionesMinusculas == 0:
+                puntuacion += -1
+        if aparicionesMinusculas == 0:
+            puntuacion += -1
+        elif aparicionesMinusculas >= 1:
+            puntuacion += 1
+        elif aparicionesMinusculas >= 3:
+            puntuacion += 3
+        
+    aparicionesDigitos = 0
+    
+    for digitos in contraseña:
+        if digitos in DIGITOS:
+            aparicionesDigitos += 1
+        if aparicionesDigitos == 0:
+            puntuacion += -2
+        elif aparicionesDigitos >= 1:
+            puntuacion += 1
+        elif aparicionesDigitos >= 3:
+            puntuacion += 2
+        elif aparicionesDigitos >= 5:
+            puntuacion += 3
+
+    aparicionesSimbolos = 0
+    for simbolo in contraseña:
+        if simbolo in SIMBOLOS:
+            aparicionesSimbolos += 1
+    if aparicionesSimbolos == 0:
+        puntuacion += -1
+    elif aparicionesSimbolos >= 1:
+        puntuacion += 1
+    elif aparicionesSimbolos >= 3:
+        puntuacion += 3
+
+    
+  
 if __name__ == "__main__":
     print(generar_contraseña_alfanumerica(8))
     print(generar_contraseña_robusta(12))
