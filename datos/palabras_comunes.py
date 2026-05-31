c00e99fb1b522f06468e173011caea0435309730
JosuuCR <jvalles.cr@gmail.com>
2026-05-31 07:25:42 -0600

    Add common words list to palabras_comunes.py

---
+++ datos/palabras_comunes.py
@@ -0,0 +1,11 @@
+PALABRAS_COMUNES = [
+    "admin", "administrator", "root", "user", "usuario",
+    "invitado", "guest", "login", "acceso", "sistema",
+    "empresa", "internet", "correo", "email", "password",
+    "passwd", "clave", "contrasena", "contraseña", "seguridad",
+    "miclave", "secreto", "secret", "private", "default",
+    "welcome", "inicio", "sesion", "sesión",
+    "pepsi", "mundo", "libro", "botella", "raul",
+    "grapadora", "optico", "huevos", "calculadora", "bolso",
+    "pollo", "cortina", "ventana", "tijeras", "lentes",
+]
