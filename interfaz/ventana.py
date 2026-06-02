a0e3dc0586289b9bd47cafb0d3a9cf75e161823f
JosuuCR <jvalles.cr@gmail.com>
2026-06-01 18:04:10 -0600

    pruebas para la GUI

--- interfaz/ventana.py
+++ interfaz/ventana.py
@@ -1,3 +1,20 @@
 import tkinter as tk
 from logica.generador import generar_contraseña_alfanumerica, generar_contraseña_robusta, generar_contraseña_desde_frase, generar_contraseña_desde_frase_aleatoria, generar_contraseña_passphrase
 from logica.evaluador import evaluar_fortaleza_contraseña
+
+ventana = tk.Tk()
+ventana.title("Gestor de Contraseñas")
+ventana.geometry("600x400")
+ventana.mainloop()
+
+
+def al_presionar():
+    etiqueta.config(text="¡Botón presionado!")
+
+
+ventana = tk.Tk()
+etiqueta = tk.Label(ventana, text="Hola mundo")
+etiqueta.pack(pady=10)
+boton = tk.Button(ventana, text="Presióname", command=al_presionar)
+boton.pack(pady=10)
+ventana.mainloop()
