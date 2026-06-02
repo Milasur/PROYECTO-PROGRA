4e8690046edcc46d9fc5b63510c58b1f11ebb2ec
JosuuCR <jvalles.cr@gmail.com>
2026-06-01 20:10:01 -0600

    Reorganize code structure in prueba.py for improved readability

--- interfaz/prueba.py
+++ interfaz/prueba.py
@@ -4,15 +4,14 @@ import tkinter as tk
 def al_presionar():
     etiqueta.config(
         text="Bienvenido a la sección de generación de contraseñas")
-    )
 
 
-        ventana = tk.Tk()
-    ventana.title("Gestor de Contraseñas")
-        ventana.geometry("600x400")
-        etiqueta = tk.Label(ventana, text="Sección Generar una contraseña")
-        etiqueta.pack(pady=10)
-        boton = tk.Button(ventana, text="Presióname", command=al_presionar)
-        boton.pack(pady=10)
+ventana = tk.Tk()
+ventana.title("Gestor de Contraseñas")
+ventana.geometry("600x400")
+etiqueta = tk.Label(ventana, text="Sección Generar una contraseña")
+etiqueta.pack(pady=10)
+boton = tk.Button(ventana, text="Presióname", command=al_presionar)
+boton.pack(pady=10)
 
-        ventana.mainloop()
+ventana.mainloop()
