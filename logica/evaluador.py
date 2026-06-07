c29c4353b1816fb195debccb9b6fc0d345321494
JosuuCR <jvalles.cr@gmail.com>
2026-06-07 12:46:41 -0600

    Corrección del orden de la funcion 8

--- logica/evaluador.py
+++ logica/evaluador.py
@@ -24,9 +24,13 @@ def evaluar_fortaleza_contraseña(contraseña):
         puntuacion += 3
     apariciones_mayusculas = 0
 
+    # Primero se cuenta la cantidad de mayusculas en la contraseña.
     for caracteres in contraseña:
         if caracteres in MAYUSCULAS:
             apariciones_mayusculas += 1
+
+    # Luego se compara y se denota la puntuacion, de mayor a menor.
+
     if apariciones_mayusculas == 0:
         puntuacion += -1
     elif apariciones_mayusculas >= 3:
@@ -39,6 +43,7 @@ def evaluar_fortaleza_contraseña(contraseña):
     for caracteres in contraseña:
         if caracteres in MINUSCULAS:
             apariciones_minusculas += 1
+
     if apariciones_minusculas == 0:
         puntuacion += -1
     elif apariciones_minusculas >= 3:
@@ -51,23 +56,25 @@ def evaluar_fortaleza_contraseña(contraseña):
     for digitos in contraseña:
         if digitos in DIGITOS:
             apariciones_digitos += 1
+
     if apariciones_digitos == 0:
         puntuacion += -1
-    elif apariciones_digitos >= 1:
-        puntuacion += 1
-    elif apariciones_digitos >= 3:
-        puntuacion += 2
     elif apariciones_digitos >= 5:
         puntuacion += 3
+    elif apariciones_digitos >= 3:
+        puntuacion += 2
+    elif apariciones_digitos >= 1:
+        puntuacion += 1
 
     apariciones_simbolos = 0
     for simbolo in contraseña:
         if simbolo in SIMBOLOS:
             apariciones_simbolos += 1
+
     if apariciones_simbolos == 0:
         puntuacion += -1
-    elif apariciones_simbolos >= 1:
-        puntuacion += 1
     elif apariciones_simbolos >= 3:
         puntuacion += 3
+    elif apariciones_simbolos >= 1:
+        puntuacion += 1
     return puntuacion
